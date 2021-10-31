5c6e974a722107c4e42248bb2486c59eb7b67366
FarhanShoukat <farhanshoukat0412@gmail.com>
2021-10-31 19:23:26 +0500

    snap moved under user input

--- Linux/Snaps.sh
+++ Linux/Snaps.sh
@@ -1,12 +1,31 @@
-sudo snap install snap-store
+#!/bin/bash
 
-sudo snap install pycharm-community --classic
-sudo snap install postman
+read -p "Snap Store (y/n): " snap_store
+read -p "PyCharm (y/n): " PYCHARM
+read -p "Postman (y/n): " POSTMAN
+read -p "Docker (y/n): " DOCKER
 
-sudo snap install docker
-sudo addgroup --system docker
-sudo adduser $USER docker
-newgrp docker
-# sudo snap disable docker
-# sudo snap enable docker
+snap_store="$(echo $snap_store | tr '[A-Z]' '[a-z]')"
+if [[ "$snap_store" != n* ]]; then
+    sudo snap install snap-store
+fi
 
+PYCHARM="$(echo $PYCHARM | tr '[A-Z]' '[a-z]')"
+if [[ "$PYCHARM" != n* ]]; then
+    sudo snap install pycharm-community --classic
+fi
+
+POSTMAN="$(echo $POSTMAN | tr '[A-Z]' '[a-z]')"
+if [[ "$POSTMAN" != n* ]]; then
+    sudo snap install postman
+fi
+
+DOCKER="$(echo $DOCKER | tr '[A-Z]' '[a-z]')"
+if [[ "$DOCKER" != n* ]]; then
+    sudo snap install docker
+    sudo addgroup --system docker
+    sudo adduser $USER docker
+    newgrp docker
+    sudo snap disable docker
+    sudo snap enable docker
+fi
